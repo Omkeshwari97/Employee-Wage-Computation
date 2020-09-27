12e25fcdb43ac8393bdd02cd64c8621ce59ff3c2
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 16:01:01 +0530

    UC6commit

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,14 +6,16 @@ public class EmployeeWageComputation
     public static final int IS_PART_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_WORK_HOURS=100;
 
 	public static void main(String args[])
 	{
-		int empHrs=0,empWage=0,totalWage=0;
+        int empHrs=0,empWage=0,totalWage=0;
+        int hours=0,days=1;
 
-        for(int i=0;i<NUM_OF_WORKING_DAYS;i++)
+        while(hours<=MAX_WORK_HOURS && days<=NUM_OF_WORKING_DAYS)
         {   
-            int empCheck=((int)Math.floor(Math.random()+15))%3;
+            int empCheck=((int)Math.floor(Math.random()+5))%3;
             //System.out.println(empCheck);
             
             switch(empCheck)
@@ -29,8 +31,13 @@ public class EmployeeWageComputation
             System.out.println("Daily Employee Wage: "+empWage);
             
             totalWage=totalWage+empWage;
+
+            hours=hours+empHrs;
+            days++;
         }
 
         System.out.println("Total Wage: "+totalWage);
+        System.out.println("Total Working Hours: "+hours);
+        System.out.println("Total working days: "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
 	}
 }
