3e5f90ba3b793e2bfd17aada521a6c648e933876
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 15:35:49 +0530

    UC1 commit

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,8 +2,15 @@ import java.util.*;
 
 public class EmployeeWageComputation
 {
+	public static final int IS_FULL_TIME=1;
+
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Employee Wage Computation");
+		double empCheck=Math.floor(Math.random()+15)%2;
+		//System.out.println(empCheck);
+		if(empCheck==IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
