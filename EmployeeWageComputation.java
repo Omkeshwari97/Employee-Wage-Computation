68c9d3d17b39b35d0ea3b6fd9ef66605f55dfb17
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 15:58:07 +0530

    UC5 commit

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,25 +4,33 @@ public class EmployeeWageComputation
 {
 	public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
+    public static final int EMP_RATE_PER_HOUR=20;
+    public static final int NUM_OF_WORKING_DAYS=20;
 
 	public static void main(String args[])
 	{
-		int empHrs=0,empWage=0;
+		int empHrs=0,empWage=0,totalWage=0;
 
-		int empCheck=((int)Math.floor(Math.random()+15))%3;
-		//System.out.println(empCheck);
-		
-        switch(empCheck)
-        {
-            case IS_FULL_TIME:  empHrs=8;
-                                break;	
-            case IS_PART_TIME:  empHrs=4;
-                                break;
-                    default:    empHrs=0;
+        for(int i=0;i<NUM_OF_WORKING_DAYS;i++)
+        {   
+            int empCheck=((int)Math.floor(Math.random()+15))%3;
+            //System.out.println(empCheck);
+            
+            switch(empCheck)
+            {
+                case IS_FULL_TIME:  empHrs=8;
+                                    break;	
+                case IS_PART_TIME:  empHrs=4;
+                                    break;
+                        default:    empHrs=0;
+            }
+
+            empWage=empHrs*EMP_RATE_PER_HOUR;
+            System.out.println("Daily Employee Wage: "+empWage);
+            
+            totalWage=totalWage+empWage;
         }
-		empWage=empHrs*EMP_RATE_PER_HOUR;
 
-        System.out.println("Daily Employee Wage: "+empWage);
-    }
+        System.out.println("Total Wage: "+totalWage);
+	}
 }
