5b8a2144eba5d9ae7ddf4f4a66a893a27046da07
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-28 20:26:18 +0530

    UC8 commit

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,11 +4,11 @@ public class EmployeeWageComputation
 {
 	public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
-    public static final int EMP_RATE_PER_HOUR=20;
-    public static final int NUM_OF_WORKING_DAYS=20;
-    public static final int MAX_WORK_HOURS=100;
+    //public static final int EMP_RATE_PER_HOUR=20;
+    //public static final int NUM_OF_WORKING_DAYS=20;
+    //public static final int MAX_WORK_HOURS=100;
 
-	public static void computeWage()
+	public static void computeWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_WORK_HOURS)
 	{
         int empHrs=0,empWage=0,totalWage=0;
         int hours=0,days=1;
@@ -36,13 +36,14 @@ public class EmployeeWageComputation
             days++;
         }
 
-        System.out.println("Total Wage: "+totalWage);
-        System.out.println("Total Working Hours: "+hours);
-        System.out.println("Total working days: "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
+        System.out.println("Total Wage of "+COMPANY_NAME+" : "+totalWage);
+        System.out.println("Total Working Hours of "+COMPANY_NAME+" : "+hours);
+        System.out.println("Total working days of "+COMPANY_NAME+" : "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
     }
     
     public static void main(String[] args)
     {
-        computeWage();
+        computeWage("Flipkart",500,25,200);
+        computeWage("Amazon",600,20,250);
     }
 }
