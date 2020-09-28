fed3ae8459965bf5dc8e0830e0b763823ba4c554
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-28 23:43:16 +0530

    UC10 commit

---
+++ CompanyEmpWage.java
@@ -0,0 +1,28 @@
+
+public class CompanyEmpWage
+{
+    public final String COMPANY_NAME;
+    public final int EMP_RATE_PER_HOUR;
+    public final int NUM_OF_WORKING_DAYS;
+    public final int MAX_WORK_HOURS;
+    public int totalWage;
+
+    public CompanyEmpWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_WORK_HOURS)
+    {
+        this.COMPANY_NAME=COMPANY_NAME;
+        this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
+        this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
+        this.MAX_WORK_HOURS=MAX_WORK_HOURS;
+    }
+
+    public void setTotalWage(int totalWage)
+    {
+        this.totalWage=totalWage;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ("Total Wage of "+COMPANY_NAME+" :"+totalWage);
+    }
+}
--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,26 +4,35 @@ public class EmployeeWageComputation
 {
 	public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
-    
-    private final String COMPANY_NAME;
-    private final int EMP_RATE_PER_HOUR;
-    private final int NUM_OF_WORKING_DAYS;
-    private final int MAX_WORK_HOURS;
+    public int n=0;
+    public CompanyEmpWage carray[];
 
-    public EmployeeWageComputation(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_WORK_HOURS)
+    public EmployeeWageComputation()
     {
-        this.COMPANY_NAME=COMPANY_NAME;
-        this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
-        this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
-        this.MAX_WORK_HOURS=MAX_WORK_HOURS;
+        carray=new CompanyEmpWage[5];
     }
 
-	public int computeWage()
+    public void addCompanyEmpWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_WORK_HOURS)
+    {
+        carray[n]=new CompanyEmpWage(COMPANY_NAME,EMP_RATE_PER_HOUR,NUM_OF_WORKING_DAYS,MAX_WORK_HOURS);
+        n++;
+    }
+
+    public void callCompute()
+    {
+        for(int i=0;i<n;i++)
+        {
+            carray[i].setTotalWage(computeWage(carray[i]));
+            System.out.println(carray[i]);
+        }
+    }
+
+	public int computeWage(CompanyEmpWage carray)
 	{
         int empHrs=0,empWage=0,totalWage=0;
         int hours=0,days=1;
 
-        while(hours<=MAX_WORK_HOURS && days<=NUM_OF_WORKING_DAYS)
+        while(hours<carray.MAX_WORK_HOURS && days<=carray.NUM_OF_WORKING_DAYS)
         {   
             int empCheck=1 + (int)(Math.random() * ((2- 1) + 1));
             //System.out.println(empCheck);
@@ -37,7 +46,7 @@ public class EmployeeWageComputation
                         default:    empHrs=0;
             }
 
-            empWage=empHrs*EMP_RATE_PER_HOUR;
+            empWage=empHrs*carray.EMP_RATE_PER_HOUR;
             System.out.println("Daily Employee Wage: "+empWage);
             
             totalWage=totalWage+empWage;
@@ -46,18 +55,17 @@ public class EmployeeWageComputation
             days++;
         }
 
-        System.out.println("Total Working Hours of "+COMPANY_NAME+" : "+hours);
-        System.out.println("Total working days of "+COMPANY_NAME+" : "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
+        System.out.println("Total Working Hours of "+carray.COMPANY_NAME+" : "+hours);
+        System.out.println("Total working days of "+carray.COMPANY_NAME+" : "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
 
         return totalWage;
     }
     
     public static void main(String[] args)
     {
-        EmployeeWageComputation c1=new EmployeeWageComputation("Flipkart",500,25,200);
-        EmployeeWageComputation c2=new EmployeeWageComputation("Amazon",600,20,250);
-
-        System.out.println("Total Wage of Flipkart: "+c1.computeWage());
-        System.out.println("Total Wage of Amazon : "+c2.computeWage());
+        EmployeeWageComputation company=new EmployeeWageComputation();
+        company.addCompanyEmpWage("Flipkart",500,25,200);
+        company.addCompanyEmpWage("Amazon",600,20,250);
+        company.callCompute();
     }
 }
